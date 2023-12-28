59cbd81319ee257cfb8913becf789ac318730b4d
Alexey Svistunov <xdayx53@gmail.com>
2023-12-28 09:24:14 +0300

    fixed footer, fixed nav, added active nav in header + burger

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ import './components/switch-toggle';
 import './components/location-buttons';
 import './components/dropdown.js';
 import './components/menu.js';
+import './components/nav.js';
--- src/js/components/switch-toggle.js
+++ src/js/components/switch-toggle.js
@@ -1,14 +1,16 @@
-const switchToggle = document.querySelector('.switch-toggle');
+const switchToggles = document.querySelectorAll('.switch-toggle');
 
-switchToggle.addEventListener('click', () =>{
-  switchToggle.classList.toggle('switch-toggle--eng');
-  if(switchToggle.classList.contains('switch-toggle--eng')) {
-    switchToggle.setAttribute('aria-checked', 'true');
-  } else {
-    switchToggle.setAttribute('aria-checked', 'false');
-  }
+switchToggles.forEach(switchToggle => {
+  switchToggle.addEventListener('click', () =>{
+    switchToggle.classList.toggle('switch-toggle--eng');
+    if(switchToggle.classList.contains('switch-toggle--eng')) {
+      switchToggle.setAttribute('aria-checked', 'true');
+    } else {
+      switchToggle.setAttribute('aria-checked', 'false');
+    }
+  });
+
 });
 
-
-const toggleElement = document.querySelector('.toggle');
-const toggleCheckbox = document.querySelector('.toggle-input');
+// const toggleElement = document.querySelector('.toggle');
+// const toggleCheckbox = document.querySelector('.toggle-input');
